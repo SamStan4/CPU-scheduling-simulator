8b5877118944a9d59b1d5231dea7ad12bd0b8156
Sam Stanley <126035220+SamStan4@users.noreply.github.com>
2024-07-10 02:27:32 -0700

    Update scheduler.c

--- scheduler.c
+++ scheduler.c
@@ -2,15 +2,8 @@
 
 int main(int argc, char *argv[])
 {
-    // Other variables
-
-    // char * test[] = {"who cares", "sample_io/input/input-3"};
-
-    // scheduler_wrapper(2, test);
 
     scheduler_wrapper(argc, argv);
-
-    // Write code for your shiny scheduler
 
     return 0;
 } 
